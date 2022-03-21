8a824ea4122041e1629b7de71ea50672b9f6d026
YunCheng <h1253567@gmail.com>
2022-03-21 20:53:15 +0800

    feat: 新增Numer轉換類別 1. int類型number 2. float類型number

--- index.ts
+++ index.ts
@@ -4,3 +4,23 @@ export const numberOrStringToComma = (val: NumOrStr) => {
   if (val === undefined) return "0";
   return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
+
+export const intNumberOrStringToComma = (val: NumOrStr) => {
+  if (val === undefined) return "0";
+  if (typeof val === "number") {
+    val = val.toString();
+  }
+  return parseInt(val)
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
+export const floatNumberOrStringToComma = (val: NumOrStr) => {
+  if (val === undefined) return "0";
+  if (typeof val === "number") {
+    val = val.toString();
+  }
+  return parseFloat(val)
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
